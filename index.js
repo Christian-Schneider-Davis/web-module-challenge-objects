650c45008d77becbc6b2a038090bd8b844341789
Christian Schneider-Davis <75326583+Christian-Schneider-Davis@users.noreply.github.com>
2020-12-11 01:03:44 -0600

    solved 3nd function

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-
+    console.log(reviews[5].feedback);
 
 
 
@@ -85,7 +85,9 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({
+  name: "Christian", rating: 10000, feedback: "He's lit."});
+console.log(reviews);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -94,6 +96,9 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays" 
+
+console.log(reviews);
 
 
 
